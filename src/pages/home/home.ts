3ec8d67473bb84192f4496faadf11786fab6979f
unknown <dam201@ACARBALLEIRA.LOCAL>
2018-11-16 14:15:50 +0100

    Codigo para coger datos externos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Proveedor1Provider } from '../../providers/proveedor1/proveedor1';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+usuarios
+  constructor(public navCtrl: NavController, public proveedor:Proveedor1Provider) {
+  }
+    ionViewDidLoad(){
+     this.proveedor.obtenerDatos()
+     .subscribe(
+       (data)=>{this.usuarios=data;},
+       (error)=>{console.log(error);}
+     )
+    }
   }
 
-}
+
